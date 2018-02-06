4da28699da53e4a782a54ea910ac043ebaa62fcb
ekirschn2018 <ekirschn@cscc.edu>
2018-02-05 19:35:05 -0500

    Updated Week 3 Exercise

--- src/com/evelynkirschner/week_3/Main.java
+++ src/com/evelynkirschner/week_3/Main.java
@@ -4,19 +4,19 @@ import java.text.DecimalFormat;
 
 public class Main {
 
-    static double fahrenheitToCelsius( double fahrenheitValue ){
-        double celsiusValue = 5.0/9 * ( fahrenheitValue - 32);
-        return celsiusValue;
+    public static double fahrenheitToCelsius( double fahrenheitValue ){
+        // Convert from Fahrenheit to Celsius using 5/9 * (Fahrenheit - 32)
+        return 5.0/9 * ( fahrenheitValue - 32);
     }
 
-    static double getFahrenheitFromUser() {
+    public static double getFahrenheitFromUser() {
         System.out.println("Enter a temperature in Fahrenheit to be converted to Celsius (-460 or less to exit)");
         java.util.Scanner scanner = new java.util.Scanner(System.in);
         String numberAsString = scanner.nextLine();
         return Double.parseDouble(numberAsString);     // Convert string to a double
     }
 
-    static void showCelsiusFromFahrenheit( double fahrenheitValue, double celsiusValue ){
+    public static void showCelsiusFromFahrenheit( double fahrenheitValue, double celsiusValue ){
         DecimalFormat decimalFormat = new DecimalFormat("#.##");    // output format to hundredths place
         System.out.println( decimalFormat.format(fahrenheitValue) + " " + '\u00b0' + "F converts to " +
                 decimalFormat.format(celsiusValue) + " " + '\u00b0' + "C" );
