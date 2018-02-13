883be7611634b3334f6a7ea2bf51bd96b377e451
ekirschn2018 <ekirschn@cscc.edu>
2018-02-12 21:01:16 -0500

    Update Week 4

--- src/com/evelynkirschner/week_4/Main.java
+++ src/com/evelynkirschner/week_4/Main.java
@@ -2,6 +2,7 @@ package com.evelynkirschner.week_4;
 
 import java.util.TreeMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Main {
     public static void getCitiesAndTemps( Map<String,String> cities ){
@@ -17,7 +18,7 @@ public class Main {
         cityName = scanner.nextLine();
         while (!( cityName.toLowerCase( ).contentEquals("end") )) {
                 // continue to get input until the user types "End"
-            System.out.println("Enter the average temperature for the next 5 days.  Separate values with a comma");
+            System.out.println("Enter the average temperature for the next 5 days.  Separate values with a space");
             cityTemps = scanner.nextLine();
             // Add the values to the map.  City Name is the key and the value is the list of temperatures
             cities.put(cityName,cityTemps);
@@ -32,32 +33,30 @@ public class Main {
     public static float calculateAverage( String numberList ){
         // method to calculate the average of a list of numbers
         // using String.split to split the string into an array of numbers
-        String numArray[5];
+        String[] numArray = new String[5];
         float numSum = 0;
-        numArray = String.split(numberList);
+        numArray = numberList.split(" ");
         for (int i = 0; i < 5; i++){
             numSum +=  Float.parseFloat(numArray[i]);     // Convert string to a float and add it to sum
         }
-        return numSum/5.0;      // Calculate the average of the 5 numbers
+        return numSum/(float)5.0;      // Calculate the average of the 5 numbers
     }
 
-    public static void displayAverages( ArrayList<String> cities ){
+    public static void displayAverages( Map<String,String> cities ){
         // method to display the average temperature
         String cityTemps;
 
-        System.out.println( "Average temperatures")
-        for (String city: cities ) {
-            cityTemps = "";
-            System.out.println( "City: " + city + " Average Temperature: " + calculateAverage(cityTemps));
-
+        System.out.println( "Average temperatures");
+        System.out.println("City" + '\u0009' + '\u0009' + "Average Temperature");
+        for (String key: cities.keySet()) {
+            System.out.println( key + '\u0009' + '\u0009' + calculateAverage(cities.get(key)));
         }
 
     }
 
     public static void main(String[] args) {
         Map<String,String> cityData = new TreeMap<String,String>();
-        getCities(cityData);
-        getTemps(cityData);
+        getCitiesAndTemps(cityData);
         displayAverages(cityData);
     }
 }
